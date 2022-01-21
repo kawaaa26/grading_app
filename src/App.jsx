604e1d71623dafb4025003eefdb938d53a5ca1f5
kawaaa26 <unic.almj@yahoo.com>
2022-01-21 19:02:37 +0900

    :lipstick:  prepared background-image and positions.
    App.js file extension renamed to as App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {Article} from "./components/index";
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrap">
+      <header>
+        <div className="video-area">
+          <video
+            className="background"
+            src="geometric_patterns.mp4"
+            muted
+            autoPlay
+            loop
+          >
+          </video>
+        </div>
+      </header>
+      <div className="container">
+        <h1>Grading Exams</h1>
+        <p>Grading students based on their Exam rasults.</p>
+        <br />
+        <Article></Article>
+      </div>
+    </div>
+  );
+}
+
+export default App;
