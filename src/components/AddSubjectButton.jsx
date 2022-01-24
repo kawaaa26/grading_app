7f8124ee867352e5a39e037889ef25de38f35677
kawaaa26 <unic.almj@yahoo.com>
2022-01-25 06:58:11 +0900

    :construction:  Implemented Duplicating Forms and Subject name.
    Still containing comments and unnecessary code among components. Remove them after recognizing current props flow among
    components.
    
    BREAKING CHANGE: Roughly implemented functionalities required in the issue. ready to eliminate unnecessary parts.
    
    Closes #3

---
+++ src/components/AddSubjectButton.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useCallback } from 'react';
+import { SubjectForm } from './index';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const AddSubjectButton = (props) => {
+  return (
+    <div>
+      <TextField
+        label="Insert Subject Name"
+        onChange={props.inputSubject}
+        variant="outlined"
+        color={props.subject.length > 0 ? "success" : "warning"}
+        inputProps={{style: {color: 'white', background: 'rgba(135, 135, 135, 0.5)', borderRadius: '5px'}}}
+        InputLabelProps={{ style: {color: 'white'}}}
+      />
+      <br />
+      <Button variant="contained" color="primary" onClick={props.handleAddSubject}>{props.subject.length > 0 ? `Add Subject: ${props.subject}` : "Add Subject"}</Button>
+    </div>
+  )
+}
+
+export default AddSubjectButton;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@
 
 export { default as Article } from './Article'
 export { default as SubjectForm } from './SubjectForm'
+export { default as AddSubjectButton } from './AddSubjectButton'
+export { default as Material } from './Material'
